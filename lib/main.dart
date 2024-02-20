1dcfff9cf59709e34953debd6476c32c3761e902
Rokhopper <slavik.utienkov13@gmail.com>
2024-02-20 15:24:54 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'screens/profile_page.dart';
+import 'package:flutter/material.dart';
+import 'screens/login_page.dart';
+import 'screens/register_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main()  async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: 'AIzaSyDBqnAH8buJ845Cbad3uKXRRACuf3Ycufk',
+        appId: '1:480297090611:android:862ec2908a358abb9a6d46',
+        messagingSenderId: '480297090611',
+        projectId: 'eaapp-72d1f')
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Authentication',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+      initialRoute: '/',
+      routes: {
+        'register': (context) => const RegisterPage(),
+        'profile': (context) => const ProfilePage(),
+
+      },
+    );
+  }
+}
